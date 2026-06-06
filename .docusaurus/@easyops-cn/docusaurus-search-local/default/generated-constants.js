a08fb233dc851dd6a0a1dd62edf80076d2d86bea
001bibhu <001bibhu@users.noreply.github.com>
2026-06-06 11:05:36 +0000

    deploy: f201a14bddad73a35ad950422edb84adcbfe3dd4

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++
@@ -1,6 +0,0 @@
-import lunr from "C:\\Users\\001bi\\my-docs-site\\node_modules\\lunr\\lunr.js";
-export const removeDefaultStopWordFilter = [];
-export const language = ["en"];
-export const searchIndexUrl = "search-index{dir}.json?_=40208143";
-export const searchResultLimits = 8;
-export const fuzzyMatchingDistance = 1;
--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("C:\\Users\\001bi\\my-docs-site\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("C:\\Users\\001bi\\my-docs-site\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\001bi\\my-docs-site\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\001bi\\my-docs-site\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\001bi\\my-docs-site\\src\\css\\custom.css"),
-];
